b5e007cbf1e5527eaa6583474d787feb44761743
Jihad-Shahariar-Joy-19-40068-1 <jihadshahariarjoy2211@gmail.com>
2022-05-29 22:04:59 +0600

    1st task CV done

---
+++ Mid/FirstWebApp/Controllers/CVController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class CVController : Controller
+    {
+        // GET: CV
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ContactInfo()
+        {
+            return View();
+        }
+        public ActionResult Objective()
+        {
+            return View();
+        }
+        public ActionResult PersonalInfo()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Skills()
+        {
+            return View();
+        }
+        public ActionResult C_Skills()
+        {
+            return View();
+        }
+        public ActionResult Experience()
+        {
+            return View();
+        }
+        public ActionResult Hobbies()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+    }
+}
